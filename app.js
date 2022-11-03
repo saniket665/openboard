ada6e78def812e646344ae2439578b159f15ca84
Aniket Singh <saniket665@gmail.com>
2022-11-03 21:10:24 +0530

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static("public"));
 
-let server = app.listen(3000, () => {
+let server = app.listen(process.env.PORT || 3000, () => {
     console.log("server started on port 3000");
 })
 
